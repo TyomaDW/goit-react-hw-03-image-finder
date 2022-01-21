2d0ab007e8f9046c85f1f33a9bacdddc8d65f56c
Tyoma <risendrums777@gmail.com>
2022-01-21 17:14:42 +0200

    Update ApiService.jsx

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -1,22 +1,15 @@
 import axios from 'axios';
 
 const apiKey = '24355682-9ff8d18cf689b459b447658d3';
-
-const instanse = axios.create();
-
-instanse.defaults.baseURL = 'https://pixabay.com/api/';
-
-instanse.defaults.params = {
-  key: apiKey,
-  orientation: 'horizontal',
-  image_type: 'photo',
-  per_page: 12,
-};
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 const ApiService = async ({ searchQuery, pageNumber }) => {
-  return await instanse
-    .get('', { params: { q: searchQuery, page: pageNumber } })
-    .then(({ data }) => data);
+  const response = await axios.get(
+    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&per_page=$12&key=${apiKey}&page=${pageNumber}`,
+  );
+
+  const data = await response;
+  return data.data;
 };
 
 export default ApiService;
